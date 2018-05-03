16801384c9389a50cc0274c04a6d7204df4cbd0f
周孝云 <zhouxiaoyun@live.cn>
2018-05-03 22:41:32 +0800

    增加系统语言

--- src/com/zhkeen/flyrise/fe/translate/utils/Constants.java
+++ src/com/zhkeen/flyrise/fe/translate/utils/Constants.java
@@ -1,6 +1,8 @@
 package com.zhkeen.flyrise.fe.translate.utils;
 
 import com.google.common.collect.ImmutableMap;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Constants {
@@ -12,7 +14,21 @@ public class Constants {
   public static final String JDBC_PORPERTIES_FILE = "src/main/resources/jdbc.properties";
   public static final String LANGUAGE_PROPERTIES_FILE = "src/main/resources/language.properties";
 
-  public static Map<String, String> ALL_LANGUAGE_MAP = ImmutableMap
-      .of("zh_CN", "简体中文", "zh_TW", "繁体中文", "en", "英文");
+  public static Map<String, String> ALL_LANGUAGE_MAP = getAllLanguageMap();
+
+  private static Map<String, String> getAllLanguageMap(){
+    Map<String, String> allMap = new LinkedHashMap<>();
+    allMap.put("zh","简体中文");
+    allMap.put("cht","繁体中文");
+    allMap.put("en","英文");
+    allMap.put("jp","日语");
+    allMap.put("kor","韩语");
+    allMap.put("fra","法语");
+    allMap.put("spa","西班牙语");
+    allMap.put("ru","俄语");
+    allMap.put("de","德语");
+    allMap.put("pt","葡萄牙语");
+    return allMap;
+  }
 
 }
