90e233576d2352198d2824622ac98cac0f03a440
周孝云 <zhouxiaoyun@live.cn>
2018-05-03 22:12:43 +0800

    修改package名称

--- src/com/zhkeen/flyrise/fe/translate/actions/TranslateHandler.java
+++ src/com/zhkeen/flyrise/fe/translate/actions/TranslateHandler.java
@@ -6,10 +6,13 @@ import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.actionSystem.EditorWriteActionHandler;
 import com.zhkeen.flyrise.fe.translate.model.TranslateResultModel;
 import com.zhkeen.flyrise.fe.translate.utils.DbUtil;
+import com.zhkeen.flyrise.fe.translate.utils.TransApi;
 import com.zhkeen.flyrise.fe.translate.utils.TranslatePluginManager;
+import com.zhkeen.flyrise.fe.translate.utils.ZHConverter;
 import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Random;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +20,8 @@ import org.slf4j.LoggerFactory;
 public class TranslateHandler extends EditorWriteActionHandler {
 
   private Logger logger = LoggerFactory.getLogger(TranslateHandler.class);
+  private static final String APP_ID = "20180503000153011";
+  private static final String SECURITY_KEY = "_MRqRxWs1i75bfvXg4kU";
 
   private final ActionHandler mHandler;
 
@@ -36,18 +41,21 @@ public class TranslateHandler extends EditorWriteActionHandler {
     if (selectedText != null && selectedText.length() > 0 && manager.isNeedMultiLanguage()) {
       try {
         DbUtil dbUtil = manager.getDbUtil();
+        TransApi transApi = new TransApi(APP_ID, SECURITY_KEY);
         TranslateResultModel model = dbUtil
             .findByMessage(manager.getDefaultLanguage(), selectedText,
                 manager.getSupportLanguageMap());
         if (model == null) {
           model = new TranslateResultModel();
-          model.setId(new Date().getTime());
+          model.setId(new Date().getTime() * 1000 + new Random().nextInt(1000));
           Map<String, String> translateMap = new LinkedHashMap<>();
           for (String lang : manager.getSupportLanguageMap().keySet()) {
             if (lang.equals(manager.getDefaultLanguage())) {
               translateMap.put(lang, selectedText);
+            } else if ("zh_TW".equals(lang)) {
+              translateMap.put(lang, ZHConverter.convert(selectedText, ZHConverter.TRADITIONAL));
             } else {
-              translateMap.put(lang, "");
+              translateMap.put(lang, transApi.getTransResult(selectedText, "auto", lang));
             }
           }
           model.setTranslateMap(translateMap);
