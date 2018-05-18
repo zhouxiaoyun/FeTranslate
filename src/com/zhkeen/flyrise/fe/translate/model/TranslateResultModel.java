95fec5c22b93c203c2aedc9b1adfdd907037f346
zhouxiaoyun <zhouxiaoyun@live.cn>
2018-05-18 22:11:25 +0800

    添加注释

--- src/com/zhkeen/flyrise/fe/translate/model/TranslateResultModel.java
+++ src/com/zhkeen/flyrise/fe/translate/model/TranslateResultModel.java
@@ -3,42 +3,57 @@ package com.zhkeen.flyrise.fe.translate.model;
 import java.sql.Date;
 import java.util.Map;
 
+/**
+ * 翻译模型
+ */
 public class TranslateResultModel {
 
-  private String id;
-  private String isJs;
-  private Map<String, String> translateMap;
-  private Date lastUpdate;
+    /**
+     * 编号
+     */
+    private String id;
+    /**
+     * 是否包含在JS文件中
+     */
+    private String isJs;
+    /**
+     * 翻译MAP，语言种类-翻译结果
+     */
+    private Map<String, String> translateMap;
+    /**
+     * 最后更新时间，方便维护时按逆序排列
+     */
+    private Date lastUpdate;
 
-  public String getId() {
-    return id;
-  }
+    public String getId() {
+        return id;
+    }
 
-  public void setId(String id) {
-    this.id = id;
-  }
+    public void setId(String id) {
+        this.id = id;
+    }
 
-  public String getIsJs() {
-    return isJs;
-  }
+    public String getIsJs() {
+        return isJs;
+    }
 
-  public void setIsJs(String isJs) {
-    this.isJs = isJs;
-  }
+    public void setIsJs(String isJs) {
+        this.isJs = isJs;
+    }
 
-  public Map<String, String> getTranslateMap() {
-    return translateMap;
-  }
+    public Map<String, String> getTranslateMap() {
+        return translateMap;
+    }
 
-  public void setTranslateMap(Map<String, String> translateMap) {
-    this.translateMap = translateMap;
-  }
+    public void setTranslateMap(Map<String, String> translateMap) {
+        this.translateMap = translateMap;
+    }
 
-  public Date getLastUpdate() {
-    return lastUpdate;
-  }
+    public Date getLastUpdate() {
+        return lastUpdate;
+    }
 
-  public void setLastUpdate(Date lastUpdate) {
-    this.lastUpdate = lastUpdate;
-  }
+    public void setLastUpdate(Date lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
 }
